6ad544c285cae9ba590f448bed3a5f36ff7c615c
joytang <qiiwiiq@hotmail.com>
2020-10-22 16:22:14 +0800

    feat: movie component

--- src/components/movie.vue
+++ src/components/movie.vue
@@ -1,20 +1,24 @@
 <template>
-  <div v-if="movieObj">
-    <div class="postoer">
-      <v-img :src="movieObj.poster" />
+  <div v-if="movieObj" class="d-flex">
+    <div class="poster">
+      <img :src="movieObj.poster" />
     </div>
-    <div class="title">{{ movieObj.title }}</div>
-    <div class="rating">{{ movieObj.rating }}</div>
-    <div class="rating_votes">{{ movieObj.rating_votes }}</div>
-    <div class="year">{{ movieObj.year }}</div>
-    <div class="length">{{ movieObj.length }}</div>
-    <div class="plot">{{ movieObj.plot }}</div>
-    <div class="cast">
-      <div v-for="(item, i) in actorList" :key="i">
-        {{ item.actor }}
+    <div>
+      <div class="d-flex">
+        <div class="title">{{ movieObj.title }}</div>
+        <div>
+          <div class="rating">{{ movieObj.rating }}</div>
+          <div class="rating_votes">{{ movieObj.rating_votes }}</div>
+        </div>
       </div>
+      <div class="d-flex">
+        <div class="year">{{ movieObj.year }}</div>
+        <div class="length">{{ movieObj.length }}</div>
+      </div>
+      <div class="cast">{{ actors }}</div>
+      <div class="plot">{{ movieObj.plot }}</div>
+      <a :href="movieObj.trailer.link" target="_blank">Watch Trailer</a>
     </div>
-    <a :href="movieObj.trailer.link" target="_blank">Watch Trailer</a>
   </div>
 </template>
 
@@ -22,13 +26,27 @@
 export default {
   props: ["movieObj"],
   computed: {
-    actorList() {
+    actors() {
       if (this.movieObj) {
-        return this.movieObj.cast.slice(0, 3);
+        let casts = this.movieObj.cast.slice(0, 3);
+        let actors = casts.map(casts => casts.actor);
+        return actors.join(", ");
       } else {
-        return [];
+        return "";
       }
     }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.poster {
+  height: 500px;
+  
+  & img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+}
+</style>
