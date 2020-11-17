d5bc7761256767efd8a03e1f3af6bdb67eeae350
joytang <qiiwiiq@hotmail.com>
2020-11-17 17:03:23 +0800

    feat: 100vh for mobile

--- src/main.js
+++ src/main.js
@@ -11,6 +11,41 @@ import VueCookies from "vue-cookies";
 import "./assets/firebase.js";
 import "./registerServiceWorker";
 
+setViewerHieght(window.innerHeight);
+
+onElementHeightChange(document.body, function (h) {
+  setViewerHieght(h);
+});
+
+function setViewerHieght(h) {
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  let vh = h * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  const bodyElem = document.getElementsByTagName('body')[0];
+  const htmlElem = document.getElementsByTagName('html')[0];
+  htmlElem.style['overflow-y'] = 'hidden';
+  htmlElem.style['height'] = '100%';
+  bodyElem.style['overflow-y'] = 'hidden';
+  bodyElem.style['height'] = '100%';
+}
+
+function onElementHeightChange(elm, callback) {
+  var lastHeight = elm.clientHeight,
+    newHeight;
+  (function run() {
+    newHeight = elm.clientHeight;
+    if (lastHeight != newHeight) {
+      callback(newHeight);
+      lastHeight = newHeight;
+    }
+    if (elm.onElementHeightChangeTimer) {
+      clearTimeout(elm.onElementHeightChangeTimer);
+    }
+    elm.onElementHeightChangeTimer = setTimeout(run, 200);
+  })();
+}
+
 Vue.config.productionTip = false;
 
 Vue.use(VueAxios, axios);
